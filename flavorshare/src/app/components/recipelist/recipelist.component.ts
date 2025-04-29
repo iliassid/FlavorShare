23335ab8e1f561f4cac104775bb72b29807329db
iliass sidqi <enaailias@gmail.com>
2025-04-29 12:35:09 +0100

    linked with routing

---
+++ flavorshare/src/app/components/recipelist/recipelist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipelist',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipelist.component.html',
+  styleUrl: './recipelist.component.scss'
+})
+export class RecipelistComponent {
+
+}
