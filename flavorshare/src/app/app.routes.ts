336f74b5306bcd51dc401faacda4548cbed9aa56
iliass sidqi <enaailias@gmail.com>
2025-04-30 16:19:32 +0100

    added the form to add recipes

--- flavorshare/src/app/app.routes.ts
+++ flavorshare/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RecipelistComponent } from './components/recipelist/recipelist.component';
 import { RecipedetailsComponent } from './components/recipedetails/recipedetails.component';
+import { RecipeformComponent } from './components/recipeform/recipeform.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "login", component: LoginComponent},
     {path: "recipelist", component: RecipelistComponent},
-    {path: "recipedetails", component: RecipedetailsComponent}
+    {path: "recipedetails", component: RecipedetailsComponent},
+    {path: "recipeform", component: RecipeformComponent}
 ];
---
+++ flavorshare/src/app/components/recipeform/recipeform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipeform',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipeform.component.html',
+  styleUrl: './recipeform.component.scss'
+})
+export class RecipeformComponent {
+
+}
