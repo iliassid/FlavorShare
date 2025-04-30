c6341cfc974a9712867f5dceb21de8f436db935f
iliass sidqi <enaailias@gmail.com>
2025-04-30 14:35:36 +0100

    done with recipelist component

--- flavorshare/src/app/app.routes.ts
+++ flavorshare/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RecipelistComponent } from './components/recipelist/recipelist.component';
+import { RecipedetailsComponent } from './components/recipedetails/recipedetails.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "login", component: LoginComponent},
-    {path: "recipelist", component: RecipelistComponent}
+    {path: "recipelist", component: RecipelistComponent},
+    {path: "recipedetails", component: RecipedetailsComponent}
 ];
